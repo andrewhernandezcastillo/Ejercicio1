313e5bebc2e87a4b7172a9166915bbe351d8ee09
Jese Rivas <jeserivasdonaires@gmail.com>
2024-08-29 20:48:22 -0500

    Mejoras en ejercicio 1

--- src/main/java/com/bank/transaction/controller/RestApi.java
+++ src/main/java/com/bank/transaction/controller/RestApi.java
@@ -1,9 +1,13 @@
 package com.bank.transaction.controller;
 
-import com.bank.transaction.service.IServices;
 import com.bank.transaction.service.Services;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController()
 public class RestApi {
